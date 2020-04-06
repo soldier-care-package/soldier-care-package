da5f0acd8ca0f2eecd7be2a4f9a91667f1e7def7
Nohemi Tarango <Nohemi5270@gmail.com>
2020-04-06 15:50:39 -0600

    wrote the outline for the persona and wrote the demographic

---
+++ epic/personas/chris.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Persona</title>
+</head>
+<body>
+<h1>Christina "Chris" Rodriguez</h1>
+
+<h2>Demographic</h2>
+<div>
+	<ul>
+		<li>Age: 24</li>
+		<li>Gender: Female</li>
+		<li>Ethnicity:</li>
+		<li>Occupation: U.S. Army</li>
+	</ul>
+</div>
+<h2>Characteristics</h2>
+<div>
+	<ul>
+		<li></li>
+	</ul>
+</div>
+<h2>Frustrations</h2>
+<div>
+	<ul>
+		<li></li>
+	</ul>
+</div>
+<h2>Positives</h2>
+<div>
+	<ul>
+		<li></li>
+	</ul>
+</div>
+<h1>User Story</h1>
+<p>As a _ user, I want to</p>
+<h1>Use Case Interaction Flow</h1>
+<div>
+	<ul>
+		<li></li>
+	</ul>
+</div>
+</body>
+</html>
