30e325720a788a749b5da40d3fa9f4a8914a34dd
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-04-06 16:32:29 -0600

    formatted sections for lilly persona

--- epic/personas/lilly.php
+++ epic/personas/lilly.php
@@ -11,15 +11,28 @@
 	<h1>Lilly Poblano</h1>
 		<p>Age: 23</p>
 		<p>Gender: Female</p>
-	<p>User Story: </p>
-		<p>Background: Lilly is a private 2nd class in the US army. She is has been deployed to Afghanistan for 2 months. She is single,
-		and has no kids. Her only close family relation back home is her father. Lilly was born and raised in Spokane, Washington. This
-		is her 2nd deployment. She finds herself missing the small comforts of home like her favorite candy, red licorice. She wished she
+		<h2>User Story:</h2>
+			<p>Lilly is a 2nd class private in the US army who is deployed to Afghanistan. She is trying to figure out a way
+			to get supplies that she needs/wants.</p>
+
+	<h2>Background: </h2>
+		<p>Lilly is has been deployed to Afghanistan for 2 months and is planed to be deployed for 6 months total. She is single,
+		and has no kids. Her only close family relation back home is her father, who is unwell. Lilly was born and raised in Spokane, Washington. This
+		is her 2nd deployment. She finds herself missing the small comforts of home like her favorite candy, red licorice. She wishes she
 		had a new book to read on her off time and a deck of cards to play with her platoon. Plus there are a few other necessities she needs
-		like thicker socks and more stationary.
-		Frustrations: Her father is the only one in her family that can send supplies to her, but is sick. She has limited access to a computer with internet. Can use the a computer at base.
-		Technology:  She has a cell-phone which is a iphone 6s that has the sim card changed, but only when she is not out on a mission. The
-		Computers on base are Dell Inspiron 22 3000 All-in-One with Windows 10 Home.
-		Goals: Lilly is needs to find another way to get supplies. She would like to find a way to get exactly what she needs.</p>
+		like thicker socks and more stationary.</p>
+
+	<h2>Frustrations:</h2>
+		<p>Her father is the only one in her family that can send supplies to her, but is sick.
+		She has limited access to a computer with internet. Is able to use a computer on base.</p>
+
+	<h2>Technology:</h2>
+		<p>She has a cell-phone which is a iphone 6s that has the sim card changed, but only when she is not out on a mission. The
+		Computers on base are Dell Inspiron 22 3000 All-in-One with Windows 10 Home.</p>
+
+	<h2>Goals:</h2>
+		<p>Lilly is needs to find another way to get supplies. She would like to find a way to get exactly what she needs.</p>
+
+	<h1>Use Case Interaction Flow:</h1>
 	</body>
 </html>
