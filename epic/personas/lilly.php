4c52e7301e0a022cef9865a96afac2a5b359c125
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-04-06 11:08:37 -0600

    lilly persona added user story

--- epic/personas/lilly.php
+++ epic/personas/lilly.php
@@ -1,2 +1,22 @@
 <!DOCTYPE html>
-<html lang="en"</html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Lilly</title>
+	</head>
+	<body>
+	<div>
+	<img src="lilly.jpg" alt="Profile picture of Lilly."
+	</div>
+	<h1>Lilly</h1>
+		<p>Age: 23</p>
+		<p>Gender: Female</p>
+		<p>User Story: Lilly is a private 2nd class in the US army. She is has been deployed to Afghanistan for 2 months. She is single,
+		and has no kids. Her only close family relation back home is her father. Lilly was born and raised in Spokane, Washington. This
+		is her 2nd deployment. She finds herself missing the small comforts of home like her favorite candy, red licorice. She wished she
+		had a new book to read on her off time and a deck of cards to play with her platoon. Plus there are a few other necesites she need
+		like thicker socks and more stationary. Her frustration is that she hates to always have to ask her father to gather thing and send
+		them to her because he is not well. She has limited access to a computer with internet. Can use the one on base. She also can use her
+		cell phone but can use only limited amount of time because of her mission. </p>
+	</body>
+</html>
