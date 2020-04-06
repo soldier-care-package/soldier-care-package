267ba64fed450c432bd9e09ad42cd35d5c9b5b9c
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-04-06 16:12:29 -0600

    editing Persona story and separting it in to sections lik background:, frustrations, technology, and goals.

--- epic/personas/lilly.php
+++ epic/personas/lilly.php
@@ -8,15 +8,18 @@
 	<div>
 	<img src="lilly.jpg" alt="Profile picture of Lilly."
 	</div>
-	<h1>Lilly</h1>
+	<h1>Lilly Poblano</h1>
 		<p>Age: 23</p>
 		<p>Gender: Female</p>
-		<p>User Story: Lilly is a private 2nd class in the US army. She is has been deployed to Afghanistan for 2 months. She is single,
+	<p>User Story: </p>
+		<p>Background: Lilly is a private 2nd class in the US army. She is has been deployed to Afghanistan for 2 months. She is single,
 		and has no kids. Her only close family relation back home is her father. Lilly was born and raised in Spokane, Washington. This
 		is her 2nd deployment. She finds herself missing the small comforts of home like her favorite candy, red licorice. She wished she
-		had a new book to read on her off time and a deck of cards to play with her platoon. Plus there are a few other necesites she need
-		like thicker socks and more stationary. Her frustration is that she hates to always have to ask her father to gather thing and send
-		them to her because he is not well. She has limited access to a computer with internet. Can use the one on base. She also can use her
-		cell phone but can use only limited amount of time because of her mission. </p>
+		had a new book to read on her off time and a deck of cards to play with her platoon. Plus there are a few other necessities she needs
+		like thicker socks and more stationary.
+		Frustrations: Her father is the only one in her family that can send supplies to her, but is sick. She has limited access to a computer with internet. Can use the a computer at base.
+		Technology:  She has a cell-phone which is a iphone 6s that has the sim card changed, but only when she is not out on a mission. The
+		Computers on base are Dell Inspiron 22 3000 All-in-One with Windows 10 Home.
+		Goals: Lilly is needs to find another way to get supplies. She would like to find a way to get exactly what she needs.</p>
 	</body>
 </html>
