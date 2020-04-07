32fd430aba0a65eb227e94fed9d6937342142c22
Nohemi Tarango <Nohemi5270@gmail.com>
2020-04-07 13:39:21 -0600

    wrote the use case interaction flow as well as the user story

--- epic/personas/chris.php
+++ epic/personas/chris.php
@@ -6,40 +6,64 @@
 </head>
 <body>
 <h1>Christina "Chris" Rodriguez</h1>
-
+<img src="alonso.jpg" alt="picture of chris">
 <h2>Demographic</h2>
 <div>
 	<ul>
 		<li>Age: 24</li>
 		<li>Gender: Female</li>
-		<li>Ethnicity:</li>
+		<li>Ethnicity: Cuban American</li>
 		<li>Occupation: U.S. Army</li>
 	</ul>
 </div>
 <h2>Characteristics</h2>
 <div>
 	<ul>
-		<li></li>
+		<li>Speaks English, Spanish, French, Italian</li>
+		<li>Likes to read</li>
+		<li>Likes to play cards and board games</li>
+		<li>Outgoing</li>
+		<li>Confident</li>
+		<li>Leader</li>
 	</ul>
 </div>
 <h2>Frustrations</h2>
 <div>
 	<ul>
-		<li></li>
+		<li>Needs things but can't always get them</li>
+		<li>Gets bored pretty easily</li>
+		<li>Feels lonely sometimes</li>
+		<li>She doesn't have much time to use the computer that they have at base</li>
 	</ul>
 </div>
 <h2>Positives</h2>
 <div>
 	<ul>
-		<li></li>
+		<li>She has an amazing team she works with</li>
+		<li>She has time to do her hobbies</li>
+		<li>She travels to different places</li>
 	</ul>
 </div>
+<h2>Technology</h2>
+<div>
+	<p>Chris has a Surface laptop 2 that she uses to get into contact with her family.</p>
+</div>
+<h2>Goals</h2>
+<div>
+	<p>Chris's goals are to be able to get the things she needs when she is stationed across seas. Her other goal is to be able to meet new people.</p>
+</div>
 <h1>User Story</h1>
-<p>As a _ user, I want to</p>
+<p>As a user, I want to be able to get the things I need and be able to talk to new people.</p>
 <h1>Use Case Interaction Flow</h1>
 <div>
 	<ul>
-		<li></li>
+		<li>Chris clicks the sign up button and the browser brings up the sign up page</li>
+		<li>Chris fills in her information and clicks sign up</li>
+		<li>The browser saves her information into their database</li>
+		<li>Chris signs in using her info and the browser searches for her info and logs her in</li>
+		<li>Chris then makes her profile and the browser saves all that she wrote</li>
+		<li>Chris makes her list and the browser saves it and posts it publicly</li>
+		<li>Chris then clicks log out and the browser takes her back to the sign in screen</li>
 	</ul>
 </div>
 </body>
