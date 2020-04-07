55fac691cc68db4abb55c27ecdb81e065d80e15d
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-04-07 16:18:46 -0600

    created Use Case Interaction Flow for lilly persona

--- epic/personas/lilly.php
+++ epic/personas/lilly.php
@@ -19,20 +19,39 @@
 		<p>Lilly is has been deployed to Afghanistan for 2 months and is planed to be deployed for 6 months total. She is single,
 		and has no kids. Her only close family relation back home is her father, who is unwell. Lilly was born and raised in Spokane, Washington. This
 		is her 2nd deployment. She finds herself missing the small comforts of home like her favorite candy, red licorice. She wishes she
-		had a new book to read on her off time and a deck of cards to play with her platoon. Plus there are a few other necessities she needs
-		like thicker socks and more stationary.</p>
+		had a new book to read on her off time and a deck of cards to play with her platoon. Plus she needs more stationary.</p>
 
 	<h2>Frustrations:</h2>
 		<p>Her father is the only one in her family that can send supplies to her, but is sick.
 		She has limited access to a computer with internet. Is able to use a computer on base.</p>
 
 	<h2>Technology:</h2>
-		<p>She has a cell-phone which is a iphone 6s that has the sim card changed, but only when she is not out on a mission. The
+		<p>She has a cell-phone which is a iphone 6s that has the sim card changed, but she can only use this when she is not out on a mission, or on duty. The
 		Computers on base are Dell Inspiron 22 3000 All-in-One with Windows 10 Home.</p>
 
 	<h2>Goals:</h2>
 		<p>Lilly is needs to find another way to get supplies. She would like to find a way to get exactly what she needs.</p>
 
 	<h1>Use Case Interaction Flow:</h1>
+		<p>Precondition: Lilly has set up an account at soldier-care-package.com</p>
+		<ul>
+			<li>Lilly logs into her account.</li>
+			<li>The Application opens up to the Home screen.</li>
+			<li>Lilly clicks the request box button.</li>
+			<li>The website brings up a form page, that has box size options up top and text box that you can enter items or put url links of the items. </li>
+			<li>She picks small box button. Then fills the text box with list: Twizzler Red Licorice, book: The Te of Piglet by Benjamin Hoff, deck of cards, a url link to stationary: https://www.amazon.com/Dickies-Multi-Pack-Dri-Tech-Moisture-Control/dp/B0757RXZJH/ref=sr_1_5?dchild=1&keywords=military%2Bsocks&qid=1586295200&sr=8-5&th=1&psc=1 (12.99),
+				and url link to pens: https://www.amazon.com/Pilot-Retractable-Premium-Roller-31078/dp/B0058NN4C0/ref=sr_1_2_sspa?dchild=1&keywords=g2+pens&qid=1586295543&sr=8-2-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFYSEgyME9NVFM3TVcmZW5jcnlwdGVkSWQ9QTA5MDcyNzAxSEVBQTVSVkQxU1ZHJmVuY3J5cHRlZEFkSWQ9QTA1MjUxNjQyWVhVQjc3RTZPRUlEJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ== (6.77).
+			Then she presses the confirm button.</li>
+			<li> The website will show a page that shows the soldiers current address listed with list underneath. There will be a change address button next to the address text, if address is not correct. Also a edit list button next to list if there needs to be any changes.
+			Then at the bottom a submit button. Text on top of the page says please confirm that details are correct.</li>
+			<li> Lilly looks over order and confirms that the address is correct and there is no edits to her list. She clicks submit.</li>
+			<li> The website will bring her back to her homepage where it will have a notification listed that her list was submitted.
+			Lilly can also delete list if ever needed as long as the box isn't selected.</li>
+			<li> Lilly is notified within the next two days that her box was selected.</li>
+			<li> Lilly is notified within the week that her box was sent.</li>
+			<li> Lilly receives her package in mail. Lilly logs on and confirms box is received.</li>
+			<li>Website moves list into received history section.</li>
+		</ul>
+		<p>Post Conditions: Lilly can send a Message back to sender if she wants to but doesn't have to. The sender in return can message Lilly if she wants to.</p>
 	</body>
 </html>
