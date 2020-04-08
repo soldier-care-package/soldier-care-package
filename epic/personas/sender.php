3bc27797babccd12ae2ea6f7d37e37b12dd9119e
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-04-08 16:25:56 -0600

    added sender file. Set basic HTML format. Started Use Case Interaction Flow

---
+++ epic/personas/sender.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Sender</title>
+	</head>
+	<body>
+		<h1> Use Case Interaction Flow:</h1>
+			<p>Precondition: # has set up an account at soldier-care-package.com.
+			<p>
+			<ul>
+			<li># logs into their account.</li>
+			<li>The Application opens up to the Home screen.</li>
+			<li># clicks on boxes button.</li>
+			<li>Web page brings them to screen of list for boxes.</li>
+			<li># Adds filter to list "small box"</li>
+			<li>Web site shows only the list for small boxes.</li>
+			<li># Looks at all the box lists and finds one that she can fulfill. Clicks on accept box</li>
+			<li>Web page brings sender to home page where the list is under "In Progress section"</li>
+			<li>
+
+				</li>
+			</ul>
+			</p>
+	</body>
+</html>
