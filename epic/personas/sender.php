ad6ed566d53154bf527f3550cfc7b67ef9778319
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-04-08 22:09:41 -0600

    completed interaction flow. added picture. Added Technology.

--- epic/personas/sender.php
+++ epic/personas/sender.php
@@ -6,7 +6,7 @@
 	</head>
 	<body>
 	<h1>Charlie Miller</h1>
-	<img src="">
+	<img src="charlie.jpg" alt="profile picture of Charlie">
 	<h2>Demographic</h2>
 	<div>
 		<ul>
@@ -41,24 +41,31 @@
 			<li>Has the financial means to help other people</li>
 		</ul>
 	</div>
+	<div>
+	<h2>Technology:</h2>
+		<p>Owns a Lenovo IdeaPad FLEX-14IWL Laptop. His phone is a Google Pixel 3a. Have Unlimited data and Hotspot acess, as well as home internet connection.</p>
+	</div>
 	<h2>User Story</h2>
 	<p>As a user, I want to help other people any way that I can.</p>
 		<h1> Use Case Interaction Flow:</h1>
-			<p>Precondition: # has set up an account at soldier-care-package.com.
+			<p>Precondition: Charlie has set up an account at soldier-care-package.com.</p>
 			<p>
 			<ul>
-			<li># logs into their account.</li>
+			<li>Charlie logs into his account.</li>
 			<li>The Application opens up to the Home screen.</li>
-			<li># clicks on boxes button.</li>
-			<li>Web page brings them to screen of list for boxes.</li>
-			<li># Adds filter to list "small box"</li>
-			<li>Web site shows only the list for small boxes.</li>
-			<li># Looks at all the box lists and finds one that she can fulfill. Clicks on accept box</li>
-			<li>Web page brings sender to home page where the list is under "In Progress section"</li>
-			<li>
-
-				</li>
+			<li>Charlie clicks on boxes button.</li>
+			<li>Web page brings up list of boxes that have been requested.</li>
+			<li>He Adds filter to list "medium box".</li>
+			<li>Web site shows only the list for medium boxes.</li>
+			<li>Charlie Looks at all the lists and finds one that he can fulfill. Clicks on accept box.</li>
+			<li>Web page brings sender to home page where the list is under "In Progress". Has options to print list or email it.</li>
+			<li>Charlie decides to view the website from his mobile phone to remember products that he has to purchase while he is at store. He gathers
+				all the products and mails box. He logs in to home page and updates status of box to mailed.</li>
+			<li> Web brings up option to type in tracking number in text box.</li>
+			<li>He types in tracking number and selects submit</li>
+			<li>Web page updates box and files it under "Mailed".</li>
 			</ul>
+	<p> Post condition: The soldier is able to update box status to received. Notifying the sender. Plus solider can send message to sender to thank them.</p>
 			</p>
 	</body>
 </html>
