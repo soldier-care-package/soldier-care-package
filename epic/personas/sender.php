59ae941ef3bfef892ab8b7d330faa8c5d31e06c5
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-04-08 21:27:13 -0600

    added Nohemi part to Charlie persona. Name, Demographic, age, Ethnicity, Characteristics, frustrations, User story

--- epic/personas/sender.php
+++ epic/personas/sender.php
@@ -2,9 +2,47 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Sender</title>
+		<title>Sender Persona</title>
 	</head>
 	<body>
+	<h1>Charlie Miller</h1>
+	<img src="">
+	<h2>Demographic</h2>
+	<div>
+		<ul>
+			<li>Age: 25</li>
+			<li>Gender: Male</li>
+			<li>Ethnicity: German American</li>
+			<li>Occupation: Writer</li>
+		</ul>
+	</div>
+	<h2>Characteristics</h2>
+	<div>
+		<ul>
+			<li>Speaks English and Spanish</li>
+			<li>Loves to read</li>
+			<li>Left Handed</li>
+			<li>Loves music</li>
+			<li>Introverted</li>
+			<li>Shy</li>
+		</ul>
+	</div>
+	<h2>Frustrations</h2>
+	<div>
+		<ul>
+			<li>Wants to help people but doesn't know how</li>
+			<li>Too shy to talk to people</li>
+		</ul>
+	</div>
+	<h2>Positives</h2>
+	<div>
+		<ul>
+			<li>He is good at writing letters</li>
+			<li>Has the financial means to help other people</li>
+		</ul>
+	</div>
+	<h2>User Story</h2>
+	<p>As a user, I want to help other people any way that I can.</p>
 		<h1> Use Case Interaction Flow:</h1>
 			<p>Precondition: # has set up an account at soldier-care-package.com.
 			<p>
