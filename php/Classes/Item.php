1456df0cd4a48b389740d40e33859d06482fff76
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-04-27 22:38:46 -0600

    get request by profileId

--- php/Classes/Item.php
+++ php/Classes/Item.php
@@ -332,6 +332,45 @@ class Item implements \JsonSerializable {
 	}
 
 	/**
+	 * gets the Request by profile id
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param Uuid|string $requestProfileId profile id to search by
+	 * @return \SplFixedArray SplFixedArray of Request found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
+	public static function getRequestByRequestProfileId(\PDO $pdo, $requestProfileId) : \SplFixedArray {
+
+		try {
+			$requestProfileId = self::validateUuid($requestProfileId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+
+		// create query template
+		$query = "SELECT requestId, requestProfileId, requestContent, requestDate FROM request WHERE requestProfileId = :requestProfileId";
+		$statement = $pdo->prepare($query);
+		// bind the request profile id to the place holder in the template
+		$parameters = ["requestProfileId" => $requestProfileId->getBytes()];
+		$statement->execute($parameters);
+		// build an array of tweets
+		$requests = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$request = new Request($row["requestId"], $row["requestProfileId"], $row["requestContent"], $row["requestDate"]);
+				$requests[$requests->key()] = $request;
+				$requests->next();
+			} catch(\Exception $exception) {
+				// if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return ($requests);
+	}
+
+	/**
 	 * formats the state variables for JSON serialization
 	 *
 	 * @return array resulting state variables to serialize
