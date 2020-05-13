17147b6e5bcff25e2c2cbad0aae3304ef6b2fc65
Nohemi Tarango <Nohemi5270@gmail.com>
2020-05-13 14:17:22 -0600

    added the first three lines for the profile api

---
+++ php/public_html/apis/profile/index.php
@@ -0,0 +1,12 @@
+<?php
+
+//require_once
+
+use Cohort28SCP\SoldierCarePackage\Profile;
+
+/**
+ * API for the Profile class
+ *
+ * @author Nohemi Tarango
+ **/
+//verify the session, start if not active
