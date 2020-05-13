66ff77d3849f15fa2c9657f2dcae38d1dad74c1c
Nohemi Tarango <Nohemi5270@gmail.com>
2020-05-13 16:01:20 -0600

    added the grab mySQL statement and the method to determing which http is being used

--- php/public_html/apis/sign-in/index.php
+++ php/public_html/apis/sign-in/index.php
@@ -8,3 +8,26 @@ require_once("/etc/apache2/capstone-mysql/Secrets.php");
 
 use Cohort28SCP\SoldierCarePackage\Profile;
 
+/**
+ * API for handling sign-in
+ *
+ * @author Nohemi Tarango
+ **/
+	//prepare an empty reply
+	$reply = new stdClass();
+	$reply->status = 200;
+	$reply->data = null;
+	try {
+		// start session
+		if(session_status() !== PHP_SESSION_ACTIVE) {
+			session_start();
+		}
+		// grab mySQL statement
+		$secrets = new \Secrets("/etc/apache2/capstone-mysql/ddctwitter.ini");
+		$pdo = $secrets->getPdoObjects();
+
+		// determine which HTTP method is being used
+		$method = array_key_exists("HTTP_X_HTTP_MEHTOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_MEHTOD"] : $_SERVER["REQUEST_METHOD"];
+
+		// If method
+	}
