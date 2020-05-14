ad5c5a4788875e90612f9a867abb525986b96bb2
Nohemi Tarango <Nohemi5270@gmail.com>
2020-05-14 10:23:20 -0600

    added the prepare an empty reply and the grab the mySQL connection

--- php/public_html/apis/activation/index.php
+++ php/public_html/apis/activation/index.php
@@ -6,3 +6,25 @@ require_once dirname(__DIR__,3) . "/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
 
 use Cohort28SCP\SoldierCarePackage\Profile;
+
+/**
+ * API to check profile activation status
+ *
+ * @author Nohemi Tarango
+ **/
+// Check the session. If it is not active, start the session.
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+// Prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+
+	// Grab the mySQL connection
+	$secrets = new \Secrets("/etc/apache2/capstone-mysql/cohort28/scp.ini");
+	$pdo = $secrets->getPdoObjects();
+
+	// Check the HTTP method being used
+}
