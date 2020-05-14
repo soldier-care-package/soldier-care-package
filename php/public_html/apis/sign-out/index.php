5a73f193e6acab947239e01093cbf903853d9190
Nohemi Tarango <Nohemi5270@gmail.com>
2020-05-13 18:18:04 -0600

    added the verification for XSRF challenge and the default error message

--- php/public_html/apis/sign-out/index.php
+++ php/public_html/apis/sign-out/index.php
@@ -5,3 +5,18 @@ require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
 
 use Cohort28SCP\SoldierCarePackage\Profile;
 
+/**
+ * API for signing out
+ *
+ * @author Nohemi Tarango
+ **/
+
+// Verify the XSRF challenge
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+// Prepare the default error message
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
