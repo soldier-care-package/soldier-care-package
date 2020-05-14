f83bc324606536314a6612b04bcab4d9aa2b0cc4
Nohemi Tarango <Nohemi5270@gmail.com>
2020-05-13 18:59:23 -0600

    added the sanitize input and the method that makes sure the id is valid for methods that require it

--- php/public_html/apis/profile/index.php
+++ php/public_html/apis/profile/index.php
@@ -26,5 +26,19 @@ try{
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 
 	// Sanitize input
+	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$profileUsername = filter_input(INPUT_GET, "profileUsername", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$profileEmail = filter_input(INPUT_GET, "profileEmail", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
+	// Make sure the id is valid for methods that require it
+	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
+		throw(new InvalidArgumentException("Id cannot be empty or negative", 405));
+	}
+
+	if($method === "GET") {
+		// Set XSRF cookie
+		setXsrfCookie();
+
+
+	}
 }
