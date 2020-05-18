a69b306a5c5ccd19ac7fef6af553457eac30917e
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-05-18 17:22:26 -0600

    added navbart

--- app/src/components/Navigation.js
+++ app/src/components/Navigation.js
@@ -6,11 +6,11 @@ export const Navigation = () => {
 	return (
 		<>
 			<Navbar bg="primary" variant="dark">
-				<Navbar.Brand href="#home">Navbar</Navbar.Brand>
+				<Navbar.Brand href="#home" >Home</Navbar.Brand>
 				<Nav className="mr-auto">
-					<Nav.Link href="#home">Home</Nav.Link>
-					<Nav.Link href="#features">Features</Nav.Link>
-					<Nav.Link href="#pricing">Pricing</Nav.Link>
+					<Nav.Link href="#home" size="lg">+</Nav.Link>
+					<Nav.Link href="#features">My Lists</Nav.Link>
+					<Nav.Link href="#pricing">Profile</Nav.Link>
 				</Nav>
 			</Navbar>
 		</>
